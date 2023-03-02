a6aba9fc46bdbe9778aa339f822ef96d63a5d7ec
Adib Hasan <adib.hasan.cse@gmail.com>
2023-03-03 00:31:53 +0600

    class13/widgets

--- app/src/main/java/com/example/practiceapp/MainActivity.kt
+++ app/src/main/java/com/example/practiceapp/MainActivity.kt
@@ -2,10 +2,36 @@ package com.example.practiceapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.practiceapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.Switch.setOnCheckedChangeListener { _, isChecked ->
+
+            if (isChecked) {
+                Toast.makeText(this, "Switch On", Toast.LENGTH_SHORT).show()
+
+            } else {
+                Toast.makeText(this, "Switch off", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        binding.checkbox.setOnCheckedChangeListener { _, isChecked ->
+
+            if (isChecked) {
+                Toast.makeText(this, "CheckBox agreed", Toast.LENGTH_SHORT).show()
+
+            } else {
+                Toast.makeText(this, "CheckBox disagreed", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
